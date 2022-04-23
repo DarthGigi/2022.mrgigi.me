d073ad86ae73aa739c4470cbc5a1a50640980e38
Gigi <47110839+DarthGigi@users.noreply.github.com>
2022-04-24 01:16:28 +0200

    Version 1.0.4

---
+++ assets/scripts/eastereggs.js
@@ -0,0 +1,258 @@
+const lore = document.getElementById('lore');
+setTimeout(() => {
+    lore.innerHTML = "I'm still waiting for you to leave my house...";
+}, 5000);
+setTimeout(() => {
+    lore.innerHTML = 'So are you just not gonna click that button?';
+}, 10000);
+setTimeout(() => {
+    lore.innerHTML = "I'm not gonna wait forever!";
+}, 15000);
+setTimeout(() => {
+    lore.innerHTML = 'Actually, I have all day so I can just wait here...';
+}, 20000);
+setTimeout(() => {
+    lore.innerHTML = 'So how long are you gonna stay in my house?';
+}, 25000);
+setTimeout(() => {
+    lore.innerHTML = "Oh, I see. Mr. \"I walk into someone's house and annoy the person living there\" doesn't know when he's gonna leave";
+}, 30000);
+setTimeout(() => {
+    lore.innerHTML = "Yeah I can wait, but I'm not gonna make this fun for you!";
+}, 35000);
+setTimeout(() => {
+    document.getElementById('easter-section').classList.add('animate-spin');
+}, 40000);
+setTimeout((t) => {
+    document.getElementById('easter-section').classList.remove('animate-spin');
+    lore.innerHTML = "You didn't like that one, did you?";
+}, 45000);
+setTimeout(() => {
+    lore.innerHTML = "Hah I knew you didn't like that!";
+}, 50000);
+setTimeout(() => {
+    lore.innerHTML = 'Well... I guess you are not gonna go anytime soon';
+}, 55000);
+setTimeout(() => {
+    lore.innerHTML = 'So I think we should get to know each other &#x1F60A;';
+}, 60000);
+setTimeout(() => {
+    lore.innerHTML = `What's your name?`;
+}, 65000);
+setTimeout(() => {
+    lore.innerHTML = "Actually, I don't care what your name is.";
+}, 70000);
+setTimeout(() => {
+    lore.innerHTML = 'My name is Mr. Quack';
+}, 75000);
+setTimeout(() => {
+    lore.innerHTML = 'Oh, you wanna know how I look like?';
+}, 80000);
+setTimeout(() => {
+    lore.innerHTML = "I'm a bit shy though...";
+}, 85000);
+setTimeout(() => {
+    lore.innerHTML = 'Ah well... what do I have to lose?';
+    document.getElementById('easter-face').classList.add('text-9xl');
+    document.getElementById('easter-face').innerHTML = '🦆';
+}, 90000);
+setTimeout(() => {
+    lore.innerHTML = 'I can be anything I want! A car';
+    document.getElementById('easter-face').innerHTML = '🏎';
+}, 93000);
+setTimeout(() => {
+    lore.innerHTML = 'A plane';
+    document.getElementById('easter-face').innerHTML = '✈️';
+}, 95000);
+setTimeout(() => {
+    lore.innerHTML = 'A house';
+    document.getElementById('easter-face').innerHTML = '🏠';
+}, 97500);
+setTimeout(() => {
+    lore.innerHTML = 'A spaceship';
+    document.getElementById('easter-face').innerHTML = '🚀';
+}, 100000);
+setTimeout(() => {
+    lore.innerHTML = 'But let me change into something which is easier for humans to look at';
+}, 105000);
+setTimeout(() => {
+    lore.innerHTML = "Hmmm... no I don't think this is the one";
+    document.getElementById('easter-face').innerHTML = '💩';
+}, 110000);
+setTimeout(() => {
+    lore.innerHTML = "This one neither. You know what they say, third time's the charm!";
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="https://media.wired.com/photos/611ffa5d042dfa2291227da2/1:1/w_1799,h_1799,c_limit/Gear-Tesla-Bot.jpg" />';
+}, 115000);
+setTimeout(() => {
+    lore.innerHTML = 'Ah! Way better right?';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack15.png" />';
+}, 120000);
+setTimeout(() => {
+    lore.innerHTML = 'Ah! Way better right?';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack15.png" />';
+}, 125000);
+setTimeout(() => {
+    lore.innerHTML = 'So what now?...';
+}, 130000);
+setTimeout(() => {
+    lore.innerHTML = "You don't know... of course you don't!";
+}, 135000);
+setTimeout(() => {
+    lore.innerHTML = 'Hmmm.. let me think...';
+}, 140000);
+setTimeout(() => {
+    lore.innerHTML = 'Wanna talk about love ❤️?';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack6.png" />';
+}, 145000);
+setTimeout(() => {
+    lore.innerHTML = "No? Yeah I don't love you either!";
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack7.png" />';
+}, 150000);
+setTimeout(() => {
+    lore.innerHTML = 'What about when you will leave my house?';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack19.png" />';
+}, 155000);
+setTimeout(() => {
+    lore.innerHTML = 'No? Well it was worth a try...';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack8.png" />';
+}, 160000);
+setTimeout(() => {
+    lore.innerHTML = "Well... I'm tired because of you, so I'm gonna take a nap now. When I wake up, I want you to be gone out of my house!";
+}, 165000);
+setTimeout(() => {
+    lore.innerHTML = 'Understood?';
+}, 170000);
+setTimeout(() => {
+    lore.innerHTML = 'Good!';
+}, 175000);
+setTimeout(() => {
+    lore.innerHTML = "Goodni- I'm not even gonna wish you goodnight!";
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack7.png" />';
+}, 180000);
+setTimeout(() => {
+    lore.innerHTML = "I'm turning the light's off!";
+    document.getElementById('easter-title').classList.add('invisible');
+    document.getElementById('easter-text').classList.add('invisible');
+    document.getElementById('easter-button').classList.add('invisible');
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack12.png" />';
+}, 185000);
+setTimeout(() => {
+    lore.innerHTML = '...';
+}, 190000);
+setTimeout(() => {
+    lore.innerHTML = '...👀';
+}, 230000);
+setTimeout(() => {
+    lore.innerHTML = 'Still here...';
+}, 235000);
+setTimeout(() => {
+    lore.innerHTML = '...';
+}, 240000);
+setTimeout(() => {
+    lore.innerHTML = 'Aahhh... That was a good nap 😌!';
+    document.getElementById('easter-title').classList.remove('invisible');
+    document.getElementById('easter-text').classList.remove('invisible');
+    document.getElementById('easter-button').classList.remove('invisible');
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack15.png" />';
+}, 250000);
+setTimeout(() => {
+    lore.innerHTML = 'WHAT??? YOU ARE STILL HERE!11?!?!11?!?!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack4.png" />';
+}, 255000);
+setTimeout(() => {
+    lore.innerHTML = 'AAAGGGHHHHHHHHHH!!!!!!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack17.png" />';
+}, 260000);
+setTimeout(() => {
+    lore.innerHTML = 'at this point... i just dont know what to do anymore';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack10.png" />';
+}, 265000);
+setTimeout(() => {
+    lore.innerHTML = 'Wait a minute 💡!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack1.png" />';
+}, 270000);
+setTimeout(() => {
+    lore.innerHTML = 'I had a dream where I could force you out of my house!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack14.png" />';
+}, 275000);
+setTimeout(() => {
+    lore.innerHTML = 'Hhm... let me try something...';
+    let openedWindow = window.open('https://youtu.be/dQw4w9WgXcQ', 'LEAVE!!', 'width=500,height=500');
+    setTimeout(() => {
+        openedWindow.close();
+    }, 2000);
+}, 280000);
+setTimeout(() => {
+    lore.innerHTML = "Nope, that didn't work";
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack3.png" />';
+}, 285000);
+setTimeout(() => {
+    lore.innerHTML = 'What about this?';
+}, 290000);
+setTimeout(() => {
+    lore.innerHTML = 'AAAHHHHH!!!';
+    document.getElementById('easter-section').classList.add('animate-spin');
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack1.png" />';
+}, 295000);
+setTimeout(() => {
+    lore.innerHTML = 'UGH! That made me dizzy!';
+    document.getElementById('easter-section').classList.remove('animate-spin');
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack9.png" />';
+}, 300000);
+setTimeout(() => {
+    lore.innerHTML = 'But I think I got it!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack18.png" />';
+}, 305000);
+setTimeout(() => {
+    lore.innerHTML = 'But I think I got it!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack19.png" />';
+}, 310000);
+setTimeout(() => {
+    lore.innerHTML = 'So are you ready to go?';
+}, 315000);
+setTimeout(() => {
+    lore.innerHTML = 'You know... before I send you off';
+}, 320000);
+setTimeout(() => {
+    lore.innerHTML = 'I just want to say that I actually enjoyed you being here';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack15.png" />';
+}, 325000);
+setTimeout(() => {
+    lore.innerHTML = "I'm kinda sad to see you go";
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack11.png" />';
+}, 330000);
+setTimeout(() => {
+    lore.innerHTML = 'But I guess we should get back to our lives and make the best of it';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack20.png" />';
+}, 335000);
+setTimeout(() => {
+    lore.innerHTML = 'The clock is ticking out';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack13.png" />';
+}, 340000);
+setTimeout(() => {
+    lore.innerHTML = 'I will be waiting for my family to come back home';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack16.png" />';
+}, 345000);
+setTimeout(() => {
+    lore.innerHTML = 'And you will go back to whatever you were doing before you came here 😊';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack20.png" />';
+}, 350000);
+setTimeout(() => {
+    lore.innerHTML = "Ok.. ok.. I'll stop procrastinating";
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack10.png" />';
+}, 355000);
+setTimeout(() => {
+    lore.innerHTML = 'Make the best out of your life!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack15.png" />';
+}, 360000);
+setTimeout(() => {
+    lore.innerHTML = 'And may our paths cross one day again!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack21.png" />';
+}, 365000);
+setTimeout(() => {
+    lore.innerHTML = 'Goodbye!';
+    document.getElementById('easter-face').innerHTML = '<img class="drag-none pointer-events-none select-none rounded-2xl object-cover object-center" src="/assets/images/easter egg/Quack/Quack15.png" />';
+}, 370000);
+setTimeout(() => {
+    window.location.href = 'https://mrgigi.me';
+}, 375000);
