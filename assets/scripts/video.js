ee86aca9342431434a1ea946d792ee8abd70ef7e
Gigi <47110839+DarthGigi@users.noreply.github.com>
2022-05-01 11:54:47 +0200

    Fixing things

---
+++ assets/scripts/video.js
@@ -0,0 +1,175 @@
+/* The code below is setting references to the video elements and buttons on the page. It is also
+adding event listeners to the buttons. */
+//set object references
+let videoCode = document.getElementById('coding')
+let togglePauseCode = document.getElementById('togglePauseCoding')
+//add event listener
+togglePauseCode.addEventListener('click', togglePauseCoding, true)
+//set object references
+let videoLife = document.getElementById('lettherebelife')
+let toggleLife = document.getElementById('toggleMuteSpace')
+let togglePauseLife = document.getElementById('togglePauseSpace')
+//add event listener
+toggleLife.addEventListener('click', toggleMuteSpace, true)
+togglePauseLife.addEventListener('click', togglePauseSpace)
+//set object references
+let videoTech = document.getElementById('wonderfultools') || null
+let toggleTech = document.getElementById('toggleMuteTechnology')
+let togglePauseTech = document.getElementById('togglePauseTechnology')
+//add event listener
+toggleTech.addEventListener('click', toggleMuteTechnology, true)
+togglePauseTech.addEventListener('click', togglePauseTechnology, true)
+/* The code below is using the Intersection Observer API to determine if the video is in the viewport.
+If it is, it will play the video. If it is not, it will pause the video. */
+const videos = document.querySelectorAll('video')
+videos.forEach((video) => {
+    // check if user prefers motion reduced
+    if (!window.matchMedia('(prefers-reduced-motion)').matches) {
+        const intersectionObserver = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        video.defaultMuted = true
+                        video.muted = true
+                        video.play()
+                    } else {
+                        video.defaultMuted = true
+                        video.muted = true
+                        video.pause()
+                    }
+                })
+            },
+            {
+                threshold: 0.01,
+            }
+        )
+        intersectionObserver.observe(video)
+    }
+})
+/* The code below is adding an event listener to the videoCode, videoLife, and videoTech variables. The
+event listener is listening for a mouseover event. When the mouseover event occurs, the
+pauseIconCode, pauseIconLife, and pauseIconTech functions are called. */
+videoCode.addEventListener('mouseover', pauseIconCode)
+videoLife.addEventListener('mouseover', pauseIconLife)
+videoTech.addEventListener('mouseover', pauseIconTech)
+/**
+ * If the video is paused, change the icon to a play button. If the video is playing, change the icon
+ * to a pause button
+ */
+function pauseIconCode() {
+    if (videoCode.paused == false) {
+        togglePauseCode.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    } else {
+        togglePauseCode.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    }
+}
+/**
+ * If the video is paused, change the icon to a play button. If the video is playing, change the icon
+ * to a pause button
+ */
+function pauseIconLife() {
+    if (videoLife.paused == false) {
+        togglePauseLife.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    } else {
+        togglePauseLife.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    }
+}
+/**
+ * If the video is paused, then the play button is displayed. If the video is playing, then the pause
+ * button is displayed.
+ */
+function pauseIconTech() {
+    if (videoTech.paused == false) {
+        togglePauseTech.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    } else {
+        togglePauseTech.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    }
+}
+/**
+ * It toggles the mute state of the videoLife element, and if the videoLife element is muted, it
+ * toggles the mute state of the videoTech element
+ */
+function toggleMuteSpace() {
+    videoTech.muted = true
+    toggleTech.innerHTML =
+        '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"></path></svg>'
+    if (videoLife.muted) {
+        videoLife.muted = false
+        toggleLife.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"></path></svg>'
+    } else {
+        videoLife.muted = true
+        toggleLife.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"></path></svg>'
+    }
+}
+/**
+ * It toggles the mute state of the technology video, and if the technology video is muted, it toggles
+ * the mute state of the life video
+ */
+function toggleMuteTechnology() {
+    videoLife.muted = true
+    toggleLife.innerHTML =
+        '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"></path></svg>'
+    if (videoTech.muted) {
+        videoTech.muted = false
+        toggleTech.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z"></path></svg>'
+    } else {
+        videoTech.muted = true
+        toggleTech.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2"></path></svg>'
+    }
+}
+/**
+ * If the video is not paused, pause it, change the button to a play button, and set the video's paused
+ * property to true. If the video is paused, play it, change the button to a pause button, and set the
+ * video's paused property to false
+ */
+function togglePauseCoding() {
+    if (videoCode.paused === false) {
+        videoCode.pause()
+        togglePauseCode.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    } else {
+        videoCode.play()
+        togglePauseCode.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    }
+}
+/**
+ * If the video is not paused, pause it and change the button to a play button. If the video is paused,
+ * play it and change the button to a pause button
+ */
+function togglePauseSpace() {
+    if (videoLife.paused === false) {
+        videoLife.pause()
+        togglePauseLife.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    } else {
+        videoLife.play()
+        togglePauseLife.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    }
+}
+/**
+ * If the video is not paused, pause it and change the button to a play button. If the video is paused,
+ * play it and change the button to a pause button
+ */
+function togglePauseTechnology() {
+    if (videoTech.paused === false) {
+        videoTech.pause()
+        togglePauseTech.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    } else {
+        videoTech.play()
+        togglePauseTech.innerHTML =
+            '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+    }
+}
