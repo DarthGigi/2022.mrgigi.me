a2e68cd142aa8a9bc778cb85af9b452b21beb506
Gigi <47110839+DarthGigi@users.noreply.github.com>
2022-05-03 17:36:20 +0200

    Version 1.1.0 - Official Release

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -47,15 +47,15 @@ function scrollIntro() {
 /* Adding an event listener to the DOMContentLoaded event. When the DOMContentLoaded event is fired, it
 will set a timeout of 1 second and then display an alert. */
 // only alert on the domain mrgigi.me
-if (window.location.hostname === 'mrgigi.me') {
-    document.addEventListener('DOMContentLoaded', () => {
-        setTimeout(() => {
-            alert(
-                '👋🏻 Thanks for visiting my website! Please note that the website is still under development and far from finished.'
-            )
-        }, 1000)
-    })
-}
+// if (window.location.hostname === 'mrgigi.me') {
+//     document.addEventListener('DOMContentLoaded', () => {
+//         setTimeout(() => {
+//             alert(
+//                 '👋🏻 Thanks for visiting my website! Please note that the website is still under development and far from finished.'
+//             )
+//         }, 1000)
+//     })
+// }
 
 // rollAnimation
 /**
